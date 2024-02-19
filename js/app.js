a2f79cbe38da9fff59c069774e5a3284a2c1ebd0
rmm21n <160491968+rmm21n@users.noreply.github.com>
2024-02-19 13:02:44 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*const nombre = prompt("Dime tu nombre");
+const Nombre = "Pepe";
+document.querySelector(".contenedor").innerHTML = `${nombre} está aprendiendo javascript.`;
+*/
+var texto = 'Esto es el contenido de la variable "texto"'
+
+let numero = 29;
+
+const PI = 3.1415;
+
+
+function saludar(){
+    alert(`texto: ${texto}; numero: ${numero}; pi: ${PI}`);
+}
